37048516aead4259aef32ac319b5e8533eef9641
Manish9811 <107361469+Manish9811@users.noreply.github.com>
2026-06-20 11:11:03 +1000

    Update server port configuration and listen condition
    Change server port to use environment variable and update listen condition.

--- server.js
+++ server.js
@@ -120,6 +120,9 @@ app.get('/', (req,res) => {
   })
 })
 
-app.listen(4000, () => {
-  console.log("Server running on http://localhost:4000");
-});
+const PORT = process.env.PORT || 5000;
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
+
+export default app;
